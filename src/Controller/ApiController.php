d0ed4a6be87137a4f9cb425c40f6b9e0aeabb918
gusu20 <gusu20@student.bth.se>
2023-04-25 11:25:52 +0200

    Redo for mvc-v2 2023

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController
+{
+    #[Route("/api/quote")]
+    public function jsonQuote(): Response
+    {
+        $quotes = array(
+            "Fear is a reaction. Courage is a decision.",
+            "You will never reach your destination if you stop and throw stones at every dog that barks.",
+            "Success consists of going from failure to failure without loss of enthusiasm."
+        );
+        $number = random_int(0, 2);
+        $date = date_create();
+        $data = [
+            'quote of the day' => $quotes[$number],
+            'date' => date('Y-m-d'),
+            'generated' => date_timestamp_get($date)
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
+    }
+}
